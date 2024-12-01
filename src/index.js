0d24b38c31a8f98666b0e89da27f1cd378fb2dea
Franco-Redivo <franco161101@gmail.com>
2024-12-01 16:39:42 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+import { project1,project2,projects, addTaskToProject, removeTaskFromProject, displayTasks, displayProjects, displayAllTasks,clearTasks, clearProjects  } from "./tasks";
+
+displayAllTasks();
+displayProjects();
+
+//buttons
+const allTasksBtn = document.querySelector('#all');
+const addProjectBtn = document.querySelector('#add-project');
+const projectBtns = document.querySelectorAll('.project-btn');
+const addTaskBtn = document.querySelector('#add-task');
+const dialog = document.querySelector('#dialog');
+const cancelTaskBtn = document.querySelector('#cancel-button');
+
+addTaskBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+cancelTaskBtn.addEventListener('click', () => {
+    dialog.close();
+});
+
+addProjectBtn.addEventListener('click', () => {
+    const projectName = prompt('Enter project name');
+    const project = { name: projectName, tasks: [] };
+    projects.push(project);
+    clearProjects();
+    displayProjects();
+});
--- src/tasks.js
+++ src/tasks.js
@@ -1,4 +1,4 @@
-
+import './styles.css';
 function Task(title, description, dueDate, priority) {
   this.title = title;
   this.description = description;
@@ -6,15 +6,26 @@ function Task(title, description, dueDate, priority) {
   this.priority = priority;
 }
 
-const task1 = new Task('Task 1', 'Description 1', '2021-12-31', 'High');
-const task2 = new Task('Task 2', 'Description 2', '2021-12-31', 'Medium');
+const projects = [];
+
+const task1 = new Task('Task 1', 'Description 1', 'dec 31th', 'High');
+const task2 = new Task('Task 2', 'Description 2', 'dec 31th', 'Medium');
+const task3 = new Task('Task 3', 'Description 3', 'nov 14th', 'Low');
+const task4 = new Task('Task 4', 'Description 4', 'nov 14th', 'High');
 
 function Project(name) {
   this.name = name;
   this.tasks = [];
 }
 
+function addProject(project) {
+  projects.push(project);
+}
+
 const project1 = new Project('Project 1');
+addProject(project1);
+const project2 = new Project('Project 2');
+addProject(project2);
 
 function addTaskToProject(project, task) {
   project.tasks.push(task);
@@ -22,6 +33,8 @@ function addTaskToProject(project, task) {
 
 addTaskToProject(project1, task1);
 addTaskToProject(project1, task2);
+addTaskToProject(project2, task3);
+addTaskToProject(project2, task4);
 
 function removeTaskFromProject(project, task) {
   project.tasks = project.tasks.filter((t) => t !== task);
@@ -29,8 +42,10 @@ function removeTaskFromProject(project, task) {
 
 const taskUl = document.querySelector('.tasks');
 const projectUl = document.querySelector('.projects');
+const projectTitle = document.querySelector('#project-title');
 
 function displayTasks(project) {
+  projectTitle.innerHTML = project.name;
   project.tasks.forEach((task) => {
     const taskLi = document.createElement('li');
     taskLi.classList.add('task');
@@ -46,17 +61,34 @@ function displayTasks(project) {
       </div>
       
     `;
+    taskUl.appendChild(taskLi);
   });
 
 }
 
-function displayProjects(projects) {
+function displayAllTasks(){
+  projects.forEach((project) => {
+    displayTasks(project);
+  });
+  projectTitle.innerHTML = 'All Tasks';
+}
+
+function displayProjects() {
   projects.forEach((project) => {
     const projectLi = document.createElement('li');
     projectLi.classList.add('project');
     projectLi.innerHTML = `
       <button class="project-btn">${project.name}</button>
     `;
+    projectUl.appendChild(projectLi);
   });
 }
+function clearTasks(){
+  taskUl.innerHTML = '';
+}
+function clearProjects(){
+  projectUl.innerHTML = '';
+}
+
+export { project1,project2,projects, addTaskToProject, removeTaskFromProject, displayTasks, displayProjects, displayAllTasks, clearTasks, clearProjects  };
 
